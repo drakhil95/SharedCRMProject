3a3899ec718c106a8d57ebea7ddb072b58c7e50d
shiva <shivain22@gmail.com>
2025-01-29 10:51:59 +0530

    Added new class

---
+++ CRM2/src/test/java/com/comcast/crm/contactTest/SampleTest.java
@@ -0,0 +1,13 @@
+package com.comcast.crm.contactTest;
+
+import org.testng.annotations.Test;
+
+public class SampleTest {
+	@Test
+	public void create()
+	{
+		System.out.println("Added new class");
+	}
+
+}
+ 
